1e321c0e01fb6c4e33d73d3604ba252befc74968
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-02-16 19:56:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function stringChop(str, size) {
   // your code here
+	let arr = [];
+  let count = 0;
+  let s = "";
+  for(let i=0;i<str.length;i++){
+      if(count<size){
+          s+=str.charAt(i);
+          count++;
+      }else if(count==size){
+          count = 0;
+          arr.push(s);
+          s="";
+      }
+  }
+  if(s.length!=0){
+      arr.push(s);
+  }
+  return arr;
 }
 
 // Do not change the code below
