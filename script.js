250910350199c0738381e0cb946693ad714253f5
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-02-17 01:18:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -22,6 +22,6 @@ function stringChop(str, size) {
 }
 
 // Do not change the code below
-// const str = prompt("Enter String.");
-// const size = prompt("Enter Chunk Size.");
+const str = prompt("Enter String.");
+const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
